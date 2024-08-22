73c44395e2a006e8eafcc3527cf66dc760c23919
Mayank Goyal <mayankgoyal020997@gmail.com>
2024-08-22 14:18:04 +0530

    Set up GitHub Actions to create and push zip file

---
+++ zipProject.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const archiver = require('archiver');
+const path = require('path');
+
+const output = fs.createWriteStream(path.join(__dirname, 'project.zip'));
+const archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
+});
+
+output.on('close', function() {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('Zip file created successfully.');
+});
+
+archive.on('error', function(err) {
+    throw err;
+});
+
+archive.pipe(output);
+
+// Append files from the root directory and node_modules
+archive.directory(__dirname, false);
+
+archive.finalize();
